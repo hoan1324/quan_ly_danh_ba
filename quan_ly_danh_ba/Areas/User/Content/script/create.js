08482bdd61dca8db8816d4346cabefa1b2082d0d
hoan1324 <hoan3397@gmail.com>
2024-07-20 22:13:24 +0700

    update

--- quan_ly_danh_ba/Areas/User/Content/script/create.js
+++ quan_ly_danh_ba/Areas/User/Content/script/create.js
@@ -3,7 +3,7 @@
     var input = $(params.input);
     // Kiểm tra nếu có ít nhất một checkbox được chọn hoặc input có giá trị khác rỗng
     return checkboxes.filter(":checked").length > 0 || input.val().trim() !== "";
-}, "Please select at least one option or fill the input.");
+}, "Vui lòng chọn ít nhất 1 lựa chọn ở mục quan hệ hoặc thêm mới nó");
 jQuery('#form-create').validate({
     rules: {
         FullName: "required",
@@ -31,6 +31,13 @@ jQuery('#form-create').validate({
             required: "Vui lòng nhập số điện thoại",
             digits: "Vui lòng nhập định dạng là số",
             minlength: "Số tối thiểu có 10 chữ số"
+        }
+    },
+    errorPlacement: function (error, element) {
+        if (element.attr("name") == "GroupName" || element.attr("name") == "newGroupName") {
+            error.insertAfter(".visible-error"); // Chèn thông báo lỗi ngay sau nhãn của "Tạo quan hệ mới"
+        } else {
+            error.insertAfter(element); // Chèn thông báo lỗi sau phần tử input khác
         }
     },
     submitHandler: function (form) {
--- quan_ly_danh_ba/Areas/User/Content/script/data-table.js
+++ quan_ly_danh_ba/Areas/User/Content/script/data-table.js
@@ -1,4 +1,4 @@
-﻿$('.delete-link').on('click', function () {
+﻿$('.delete-item').on('click', function () {
     const itemid = $(this).data('id');
     $('#confirmdeletebtn').attr('href', `/user/contact/delete?id=${itemid}`); // cập nhật link với id
 });
