3f99a6c9c21dbc138daa18b1838da734e4f977ee
hoan1324 <hoan3397@gmail.com>
2024-07-29 16:39:30 +0700

    update

---
+++ quan_ly_danh_ba/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quan_ly_danh_ba.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+       public ActionResult SignIn()
+        {
+            return View();
+        }
+        public ActionResult SignUp() {
+        return View();
+        }
+    }
+}
